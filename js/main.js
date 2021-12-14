34a608022c7fcef999d1e48186bdad95629f3235
Mariana Viana Rodriguez <workdeveloper.new@gmail.com>
2021-12-14 09:02:24 +0100

    added html, css, js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+//Elementos
+
+const selectOptionElements = document.querySelector('.js-select');
+const inputBtnElement = document.querySelector('.button');
+const textGoElement = document.querySelector('.js-start');
+const textPlayerElement = document.querySelector('.js_cont_player');
+const textComputerElement = document.querySelector('.js_cont_computer');
